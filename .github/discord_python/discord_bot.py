af4e8eb372c10c34f6fc656bb1cd1d2ffb4c4637
Video-gamer <mkrabsde@gmail.com>
2021-11-11 22:03:49 +0100

    cicd: everything should work now

--- .github/discord_python/discord_bot.py
+++ .github/discord_python/discord_bot.py
@@ -21,10 +21,10 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     channel = client.get_channel(799575937984495630)
 
-    embed = discord.Embed(title=(('success.png', 'fail.png')[args['Success'] == 'success']) + ' deployed 13TRIS',
-                          color=(0x04b30d, 0xb200000)[args['Success'] == 'success'])
+    embed = discord.Embed(title=(('Successfully', 'Unsuccessfully')[args['Success'] == 'success']) + ' deployed 13TRIS',
+                          color=(0x04b30d, 0xb20000)[args['Success'] == 'success'])
     embed.set_author(name='GitHub - {0.user}'.format(client), url='https://mkrabs.duckdns.org',
-                     icon_url='.github/discord_python/' + (('success.png', 'fail.png')[args['Success'] == 'success']))
+                     icon_url='https://pics.freeicons.io/uploads/icons/png/' + (('16618812301557740370-32.png', '6549974331557740369-32.png')[args['Success'] == 'success']))
     embed.add_field(name='Branch', value=args['Branch'], inline=True)
     embed.add_field(name='Time', value=args['Time'], inline=True)
     embed.add_field(name='Pipeline', value=args['Pipeline'], inline=True)
