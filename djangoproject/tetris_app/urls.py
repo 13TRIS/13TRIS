7a31e0152558e84580d482ca1596151d51843673
A002727 <danielmajer.1@web.de>
2021-12-05 16:39:11 +0100

    homepage, faq, impressum

--- djangoproject/tetris_app/urls.py
+++ djangoproject/tetris_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import login_view, register_view, homepage_view
+from .views import login_view, register_view, homepage_view, impressum_view, FAQ_view
 from django.views.generic import RedirectView
 
 urlpatterns = [
     path('', RedirectView.as_view(url='/login')),  # for going to the login view even if no url path is specified
     path('login', login_view),
     path('register', register_view),
-    path('home', homepage_view)
+    path('home', homepage_view),
+    path('impressum', impressum_view),
+    path('faq', FAQ_view)
 ]
--- djangoproject/tetris_app/views.py
+++ djangoproject/tetris_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 
 # Create your views here.
+
+
 def login_view(request):
     return render(request, 'tetris_app/login-view.html')
 
@@ -13,3 +15,11 @@ def register_view(request):
 # This has to be removed eventually
 def homepage_view(request):
     return render(request, 'tetris_app/homepage-view.html')
+
+# This has to be removed eventually
+def FAQ_view(request):
+    return render(request, 'tetris_app/FAQ-view.html')
+
+# This has to be removed eventually
+def impressum_view(request):
+    return render(request, 'tetris_app/impressum-view.html')
