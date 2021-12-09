458da4d45d024636e2648c75675af47d01361a38
entrisity <entrisity@gmail.com>
2021-12-09 11:36:41 +0100

    backend-beta: login and logout, username shows on homepage

--- djangoproject/tetris_app/urls.py
+++ djangoproject/tetris_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import login_view, register_view, homepage_view, impressum_view, faq_view, datenschutz_view, game_solo_view
+from .views import login_view, logout_user, register_view, homepage_view, impressum_view, faq_view, datenschutz_view, game_solo_view
 from django.views.generic import RedirectView
 
 urlpatterns = [
     path('', RedirectView.as_view(url='/login')),  # for going to the login view even if no url path is specified
     path('login', login_view, name='login'),
+    path('logout', logout_user, name='logout'),
     path('register', register_view),
     path('home', homepage_view, name='home'),
     path('impressum', impressum_view),
--- djangoproject/tetris_app/views.py
+++ djangoproject/tetris_app/views.py
@@ -1,31 +1,49 @@
 from django.contrib.auth import authenticate
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
-from .models import *
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
 
 
 def login_view(request):
+    context = {}
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-        user = User.objects.get(name=username, password=password)
+
+        user = authenticate(request, username=username, password=password)
+
         if user is not None:
-            print("passt")
+            login(request, user)
             return redirect('home')
+        else:
+            messages.info(request, 'Username or password is incorrect')
+            return render(request, 'tetris_app/login-view.html')
     return render(request, 'tetris_app/login-view.html')
 
 
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
+
 def register_view(request):
-    if request.method == 'POST':
-        print(request.POST.get('username'))
-        print(request.POST.get('password'))
-        user = User(name=request.POST.get('username'), password=request.POST.get('password1'))
-        user.save()
-        return redirect('login')
-    return render(request, 'tetris_app/register-view.html')
+    form = UserCreationForm()
+    context = {'form': form}
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        print("post")
+        if form.is_valid():
+            print("valid")
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'An account has been created for ' + user)
+            form.save()
+            return redirect('login')
+    return render(request, 'tetris_app/register-view.html', context)
 
 
 # This has to be removed eventually
