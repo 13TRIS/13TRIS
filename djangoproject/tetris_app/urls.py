dc455b1aac88486604922897bfaa1f9c297d4299
Felix Gervasi <72347875+fgervasi-cell@users.noreply.github.com>
2021-12-12 06:34:49 +0100

    Backend v2 (#10)
    * backend: register works with database, check for login also works.
    
    * backend-beta: login and logout, username shows on homepage
    
    * backend-v2: Used base template for home and play views
    
    * Frontend: register validation and Username availability check
    
    * Frontend: validation now as file under registry.js
    
    * Frontend: Username validation also work in the login page. Validation only runs after 1 second of not writing
    
    * backend-v2: Tried adding friends list feature
    
    * backend-v2: Friends list works now
    
    * backend-v2: fixed issue where site would break if the user had no friends record in the databse. Also Anonymus can't update friends
    
    * backend-v2: fixed #89
    
    * backend-v2: fixed #93 - alert wrong color
    
    * base.html
    
    * backend-v2: fixed #97 Guest in game canvas, and Added user status in friends list
    
    * base.html
    
    * base, game-solo
    
    * base, homepage
    
    * base, homepage
    
    Co-authored-by: entrisity <entrisity@gmail.com>
    Co-authored-by: Marc <71791731+MKrabs@users.noreply.github.com>
    Co-authored-by: A002727 <danielmajer.1@web.de>

--- djangoproject/tetris_app/urls.py
+++ djangoproject/tetris_app/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
-from .views import login_view, register_view, homepage_view, impressum_view, faq_view, datenschutz_view, game_solo_view
+from . import views
 from django.views.generic import RedirectView
 
 urlpatterns = [
     path('', RedirectView.as_view(url='/login')),  # for going to the login view even if no url path is specified
-    path('login', login_view),
-    path('register', register_view),
-    path('home', homepage_view),
-    path('impressum', impressum_view),
-    path('faq', faq_view),
-    path('datenschutz',datenschutz_view),
-    path('game-solo', game_solo_view)
+    path('login', views.login_view, name='login'),
+    path('logout', views.logout_user, name='logout'),
+    path('register', views.register_view),
+    path('home', views.homepage_view, name='home'),
+    path('impressum', views.impressum_view),
+    path('faq', views.faq_view),
+    path('datenschutz',views.datenschutz_view),
+    path('game-solo', views.game_solo_view),
+    path('validate_username', views.validate_username, name='validate_username'),
+    path('connect/<str:operation>/<str:username>', views.update_friend, name='update_friend')
 ]
--- djangoproject/tetris_app/views.py
+++ djangoproject/tetris_app/views.py
@@ -1,32 +1,109 @@
-from django.shortcuts import render
-
-
-# Create your views here.
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from .models import Friend, Profile
+from django.views.generic import TemplateView
 
 
 def login_view(request):
+    context = {}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.warning(request, 'Username or password is incorrect')
+            return render(request, 'tetris_app/login-view.html')
     return render(request, 'tetris_app/login-view.html')
 
 
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
+
 def register_view(request):
-    return render(request, 'tetris_app/register-view.html')
+    form = UserCreationForm()
+    context = {'form': form}
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        print("post")
+        if form.is_valid():
+            print("valid")
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'An account has been created for ' + user)
+            form.save()
+            return redirect('login')
+    return render(request, 'tetris_app/register-view.html', context)
+
+
+def validate_username(request):
+    """Check username availability"""
+    username = request.GET.get('username', None)
+    print(len(username.strip()))
+    taken = len(username.strip()) < 1 or User.objects.filter(username__iexact=username).exists()
+    response = {
+        'is_taken': taken
+    }
+    return JsonResponse(response)
+
 
 # This has to be removed eventually
 def homepage_view(request):
-    return render(request, 'tetris_app/homepage-view.html')
+    try:
+        if not request.user.is_authenticated:
+            raise ObjectDoesNotExist
+        friend = Friend.objects.get(current_user=request.user)
+        friends = friend.users.all()
+        print(f'zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz {str(friend.users.count())}')
+
+        args = {
+            'friends': friends
+        }
+        return render(request, 'tetris_app/homepage-view.html', args)
+    except ObjectDoesNotExist:
+        return render(request, 'tetris_app/homepage-view.html')
+
 
 # This has to be removed eventually
 def faq_view(request):
     return render(request, 'tetris_app/FAQ-view.html')
 
+
 # This has to be removed eventually
 def impressum_view(request):
     return render(request, 'tetris_app/impressum-view.html')
+
 
 # This has to be removed eventually
 def datenschutz_view(request):
     return render(request, 'tetris_app/datenschutz-view.html')
 
+
 # This has to be removed eventually
 def game_solo_view(request):
     return render(request, 'tetris_app/game-solo-view.html')
+
+
+def update_friend(request, operation, username):
+    try:
+        if request.user.is_authenticated:
+            new_friend = User.objects.get(username=username)
+            print(type(new_friend))
+            if operation == 'add':
+                Friend.make_friend(request.user, new_friend)
+            if operation == 'remove':
+                Friend.lose_friend(request.user, new_friend)
+    except:
+        pass
+    return redirect('home')
