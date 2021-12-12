6429fdc4cbe5aed22c85d911d218a2d499e83f28
fgervasi-cell <gervasi.felix@student.dhbw-karlsruhe.de>
2021-12-12 09:58:44 +0100

    TETRIS-91: Input fields are weirdly arranged as well as the corresponding error messages

---
+++ djangoproject/tetris_app/static/tetris_app/AddInputStyleOnLoad.js
@@ -0,0 +1,5 @@
+window.onload = () => {
+    document.querySelector('#id_username').setAttribute('class', 'form-control');
+    document.querySelector('#id_password1').setAttribute('class', 'form-control');
+    document.querySelector('#id_password2').setAttribute('class', 'form-control');
+}
