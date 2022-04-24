eb23c2c776ca46a92f9edb02f18f5302db95f9d4
fgervasi-cell <gervasi.felix@student.dhbw-karlsruhe.de>
2022-04-24 17:50:19 +0200

    TETRIS-140: Make sure that people that leave the lobby creation site get removed from the lobby
    It almost works

--- djangoproject/tetris_app/server.py
+++ djangoproject/tetris_app/server.py
@@ -1,69 +1,103 @@
 import asyncio
 import json
 import secrets
+import time
+from StoppableThread import StoppableThread
 
 import websockets
 from websockets.exceptions import ConnectionClosedOK
 
 CONNECTED = {}
 LOBBIES = {}
+THREADS = {}
 
 
 async def handler(websocket):
     try:
         async for message in websocket:
             event = json.loads(message)
-            print(message)
             if event["type"] == "invite":
+                print(event["from"] + " invited " + event["to"])
                 await CONNECTED[event["to"]].send(message)
             elif event["type"] == "init":
-                lobby_id = secrets.token_urlsafe(12)
-                CONNECTED[event["user"]] = websocket
-                LOBBIES[lobby_id] = {event["user"]}, event["user"]
-                init_event = {
-                    "type": "init",
-                    "lobby_id": lobby_id,
-                }
-                await websocket.send(json.dumps(init_event))
+                await receive_init(websocket, event)
             elif event["type"] == "join":
-                # get the list of users associated with the lobby id from which the invite request was received
-                lobby, admin = LOBBIES[event["lobby"]]
-                # add the user to this list if he is not part of the lobby already
-                if event["user"] not in lobby:
-                    join_event = {
-                        "type": "join",
-                        "user": event["user"],
-                    }
-                    for user in lobby:
-                        socket = CONNECTED[user]
-                        await socket.send(json.dumps(join_event))
-                    # needs to be added afterwards -> otherwise KeyError!
-                    lobby.add(event["user"])
+                await receive_join(event)
             elif event["type"] == "leave":
-                lobby, admin = LOBBIES[event["lobby"]]
-                if len(lobby) <= 1:
-                    del LOBBIES[event["lobby"]]
-                    print(LOBBIES)
-                else:
-                    for user in lobby:
-                        if user == event["user"]:
-                            del user
+                thread = StoppableThread(target=leave_lobby, args=(event,))
+                THREADS[event["user"]] = thread
+                thread.start()
             elif event["type"] == "update":
+                if event["user"] in THREADS:
+                    THREADS[event["user"]].stop()
                 CONNECTED[event["user"]] = websocket
             elif event["type"] == "request":
-                lobby, admin = LOBBIES[event["lobby"]]
-                lobby_info = {
-                    "type": "lobby-info",
-                    "lobby": list(lobby),
-                    "admin": admin,
-                }
-                await websocket.send(json.dumps(lobby_info))
+                await send_lobby_info(websocket, event)
     except ConnectionClosedOK:
         pass
     finally:
         for key in list(CONNECTED.keys()):
             if CONNECTED[key] == websocket:
                 del CONNECTED[key]
+
+
+async def receive_init(websocket, event):
+    lobby_id = secrets.token_urlsafe(12)
+    CONNECTED[event["user"]] = websocket
+    LOBBIES[lobby_id] = {event["user"]}, event["user"]
+    init_event = {
+        "type": "init",
+        "lobby_id": lobby_id,
+    }
+    print("lobby " + lobby_id + " created for " + event["user"])
+    await websocket.send(json.dumps(init_event))
+
+
+async def receive_join(event):
+    # get the list of users associated with the lobby id from which the invite request was received
+    lobby, admin = LOBBIES[event["lobby"]]
+    # add the user to this list if he is not part of the lobby already
+    if event["user"] not in lobby:
+        join_event = {
+            "type": "join",
+            "user": event["user"],
+        }
+        for user in lobby:
+            socket = CONNECTED[user]
+            await socket.send(json.dumps(join_event))
+        # needs to be added afterwards -> otherwise KeyError!
+        lobby.add(event["user"])
+    print(event["user"] + " joined " + str(LOBBIES[event["lobby"]]))
+
+
+def leave_lobby(event):
+    print(THREADS)
+    # wait 5 seconds because something might come in from the user if he did not leave the site
+    time.sleep(5)
+    if THREADS[event["user"]].stopped():
+        del THREADS[event["user"]]
+        return
+    # if we do not get an update request in those 5 second the player has left the page and we can delete
+    lobby, admin = LOBBIES[event["lobby"]]
+    if len(lobby) <= 1:
+        del LOBBIES[event["lobby"]]
+    else:
+        for user in lobby:
+            if user == event["user"]:
+                del user
+    del THREADS[event["user"]]
+    print(event["user"] + " left lobby " + event["lobby"])
+
+
+async def send_lobby_info(websocket, event):
+    lobby, admin = LOBBIES[event["lobby"]]
+    lobby_info = {
+        "type": "lobby-info",
+        "lobby": list(lobby),
+        "admin": admin,
+    }
+    print("info about lobby " + str(LOBBIES[event["lobby"]]) + " was sent")
+    await websocket.send(json.dumps(lobby_info))
 
 
 async def main():
--- djangoproject/tetris_app/static/tetris_app/js/client.js
+++ djangoproject/tetris_app/static/tetris_app/js/client.js
@@ -4,27 +4,18 @@ window.addEventListener("DOMContentLoaded", () => {
     const lobby_id = new URLSearchParams(window.location.search).get("lobby");
     const websocket = new WebSocket("ws://localhost:8001");
     let modal = new bootstrap.Modal(document.querySelector("#invitation-modal"));
-    let leaveModal = new bootstrap.Modal(document.querySelector("#leave-modal"));
     init(websocket, lobby_id);
     sendInvite(websocket, lobby_id);
     acceptInvite(websocket, modal, lobby_id);
     receive(websocket, modal, lobby_id);
-    beforeUnload(websocket, lobby_id, leaveModal);
+    beforeUnload(websocket, lobby_id);
 });
 
-function beforeUnload(websocket, lobby_id, modal) {
+function beforeUnload(websocket, lobby_id) {
     if (!lobby_id)
         return;
-    window.addEventListener("beforeunload", (event) => {
-        event.preventDefault();
-        return event.returnValue = "Do you really want to leave the lobby?";
-    });
-    window.addEventListener("close", () => {
-       leaveLobby(websocket, lobby_id);
-    });
-    document.getElementById("leave-confirm").addEventListener("click", () => {
-       leaveLobby(websocket, lobby_id);
-       window.location.replace(window.location.href.split("?")[0]);
+    window.addEventListener("beforeunload", () => {
+        leaveLobby(websocket, lobby_id);
     });
 }
 
@@ -36,7 +27,7 @@ function acceptInvite(websocket, modal, lobby_id) {
             "lobby": INVITED_TO,
         }
         modal.hide();
-        leaveLobby(websocket, lobby_id);
+        //leaveLobby(websocket, lobby_id);
         websocket.send(JSON.stringify(joinEvent));
         window.location.replace(window.location.href.split("?")[0] + "?lobby=" + INVITED_TO);
     });
@@ -95,7 +86,6 @@ function init(websocket, lobby_id) {
         "type": "init",
         "user": user
     }
-    console.log(lobby_id);
     websocket.addEventListener("open", () => {
         if (!lobby_id) {
             websocket.send(JSON.stringify(init));
