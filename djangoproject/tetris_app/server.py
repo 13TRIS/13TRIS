d476a6d5056997cb67b69b7d7fc8c36bac35bded
fgervasi-cell <gervasi.felix@student.dhbw-karlsruhe.de>
2022-04-29 09:40:29 +0200

    TETRIS-140: Make sure that people that leave the lobby creation site get removed from the lobby
    Fix?

--- djangoproject/tetris_app/server.py
+++ djangoproject/tetris_app/server.py
@@ -48,12 +48,7 @@ async def receive_init(websocket, event):
     lobby_id = secrets.token_urlsafe(12)
     CONNECTED[event["user"]] = websocket
     LOBBIES[lobby_id] = {event["user"]}, event["user"]
-    init_event = {
-        "type": "init",
-        "lobby_id": lobby_id,
-    }
-    print("lobby " + lobby_id + " created for " + event["user"])
-    await websocket.send(json.dumps(init_event))
+    await websocket.send(json.dumps(init_event(lobby_id)))
 
 
 async def receive_join(event):
@@ -61,22 +56,16 @@ async def receive_join(event):
     lobby, admin = LOBBIES[event["lobby"]]
     # add the user to this list if he is not part of the lobby already
     if event["user"] not in lobby:
-        join_event = {
-            "type": "join",
-            "user": event["user"],
-        }
         for user in lobby:
             socket = CONNECTED[user]
-            await socket.send(json.dumps(join_event))
+            await socket.send(json.dumps(join_event(event["user"])))
         # needs to be added afterwards -> otherwise KeyError!
         lobby.add(event["user"])
-    print(event["user"] + " joined " + str(LOBBIES[event["lobby"]]))
 
 
 def leave_lobby(event, is_instant):
     # wait 5 seconds because something might come in from the user if he did not leave the site
-    time.sleep(1)
-    print(event["user"] + " left lobby " + event["lobby"])
+    time.sleep(5)
     if not is_instant and THREADS[event["user"]].stopped():
         del THREADS[event["user"]]
         return
@@ -91,33 +80,31 @@ def leave_lobby(event, is_instant):
                 lobby.discard(user)
             else:
                 websockets_in_lobby.add(CONNECTED[user])
-        lobby_info = {
-            "type": "lobby_info",
-            "lobby": list(lobby),
-            "admin": admin
-        }
-        websockets.broadcast(websockets_in_lobby, json.dumps(lobby_info))
+        websockets.broadcast(websockets_in_lobby, json.dumps(lobby_info(lobby, admin)))
         if not is_instant:
             del THREADS[event["user"]]
-    print(THREADS)
-    print(LOBBIES)
-    print(CONNECTED)
 
 
 async def send_lobby_info(websocket, event):
     lobby, admin = LOBBIES[event["lobby"]]
-    lobby_info = {
-        "type": "lobby-info",
-        "lobby": list(lobby),
-        "admin": admin,
-    }
-    print("info about lobby " + str(LOBBIES[event["lobby"]]) + " was sent")
-    await websocket.send(json.dumps(lobby_info))
+    await websocket.send(json.dumps(lobby_info(lobby, admin)))
 
 
 async def main():
     async with websockets.serve(handler, "", 8001):
         await asyncio.Future()
+
+
+def init_event(lobby_id):
+    return {"type": "init", "lobby_id": lobby_id}
+
+
+def lobby_info(lobby, admin):
+    return {"type": "lobby-info", "lobby": list(lobby), "admin": admin}
+
+
+def join_event(user):
+    return {"type": "join", "user": user}
 
 
 if __name__ == "__main__":
