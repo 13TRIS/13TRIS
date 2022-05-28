c9042971e8ae0f2df3d65c584d126a0b782f0656
fgervasi-cell <gervasi.felix@student.dhbw-karlsruhe.de>
2022-05-28 11:15:45 +0200

    TETRIS-175 Refactoring client.js

--- djangoproject/tetris_app/static/js/client.js
+++ djangoproject/tetris_app/static/js/client.js
@@ -1,26 +1,387 @@
-var INVITED_TO = null;
+// Stores all possible events that a client can send to the websocket server.
+const ClientEvents = (function () {
+    /***
+     * Event that is sent to the server to generate a lobby for the sender.
+     * @param {string} user
+     * @returns {{type: string, user: string}}
+     */
+    const initEvent = function (user) {
+        return {
+            "type": "init",
+            "user": user,
+        }
+    }
+    /***
+     * Event that is sent to notify another user that he should join the lobby.
+     * @param {string} lobbyId
+     * @param {string} to
+     * @param {string} user
+     * @returns {{lobby: string, from: string, to: string, type: string}}
+     */
+    const inviteEvent = function (lobbyId, to, user) {
+        return {
+            "type": "invite",
+            "lobby": lobbyId,
+            "to": to,
+            "from": user,
+        }
+    }
+    /***
+     * Event that is sent from someone who was invited into a lobby and wants to join this lobby.
+     * @param {string} invitedTo
+     * @param {string} user
+     * @returns {{lobby: string, type: string, user: string}}
+     */
+    const joinEvent = function (invitedTo, user) {
+        return {
+            "type": "join",
+            "user": user,
+            "lobby": invitedTo,
+        }
+    }
+    /***
+     * Event that is sent to the server to remove the user from a lobby.<br>
+     * If 'isInstant'=true he will immediately be removed.<br>
+     * If 'isInstant'=false the server will start a thread that checks if an 'updateConnectionEvent' is sent from
+     * the client afterwards. If so he will not be removed.<br>
+     * If 'isKick'=true a new lobby will be created for user 'userName'.
+     * @param {string} lobbyId
+     * @param {string} userName
+     * @param {boolean} isInstant
+     * @param {boolean} isKick
+     * @returns {{lobby: string, kick: boolean, type: string, user: string, instant: boolean}}
+     */
+    const leaveEvent = function (lobbyId, userName, isInstant, isKick) {
+        return {
+            "type": "leave",
+            "lobby": lobbyId,
+            "user": userName,
+            "instant": isInstant,
+            "kick": isKick,
+        }
+    }
+    /***
+     * Tells the server to update the connection for the given 'user'.
+     * @param {string} user
+     * @returns {{type: string, user: string}}
+     */
+    const updateConnectionEvent = function (user) {
+        return {
+            "type": "update",
+            "user": user,
+        }
+    }
+    /***
+     * Requests information about the lobby 'lobbyId' from the server.
+     * @param {string} lobbyId
+     * @returns {{lobby: string, type: string}}
+     */
+    const lobbyRequestEvent = function (lobbyId) {
+        return {
+            "type": "request",
+            "lobby": lobbyId,
+        }
+    }
 
-window.addEventListener("DOMContentLoaded", () => {
-    const lobby_id = new URLSearchParams(window.location.search).get("lobby");
+    return {
+        initEvent: initEvent,
+        inviteEvent: inviteEvent,
+        joinEvent: joinEvent,
+        leaveEvent: leaveEvent,
+        updateConnectionEvent: updateConnectionEvent,
+        lobbyRequestEvent: lobbyRequestEvent,
+    }
+})();
+
+// Represents a client who can communicate with a websocket server.
+const Client = (function () {
+    let invitedTo = null;
+
+    /***
+     * Sends an 'ClientEvents.initEvent' to the server at the specified 'websocket' connection.
+     * @param {WebSocket} websocket
+     * @param {string} user
+     */
+    const sendInitEvent = function (websocket, user) {
+        websocket.send(JSON.stringify(ClientEvents.initEvent(user)));
+    }
+    /***
+     * Sends an 'ClientEvents.inviteEvent' to the server.
+     * @param {WebSocket} websocket
+     * @param {string} lobbyId
+     * @param {string} to
+     * @param {string} user
+     */
+    const sendInviteEvent = function (websocket, lobbyId, to, user) {
+        websocket.send(JSON.stringify(ClientEvents.inviteEvent(lobbyId, to, user)));
+    }
+    /***
+     * Lets 'user' join the lobby 'invitedTo'.
+     * @param {WebSocket} websocket
+     * @param {string} invitedTo
+     * @param {string} user
+     */
+    const sendJoinEvent = function (websocket, invitedTo, user) {
+        websocket.send(JSON.stringify(ClientEvents.joinEvent(invitedTo, user)));
+    }
+    /***
+     * Sends a 'ClientEvents.leaveEvent' to the server with the given arguments.
+     * @param {WebSocket} websocket
+     * @param {string} lobbyId
+     * @param {string} userName
+     * @param {boolean} isInstant
+     * @param {boolean} isKick
+     */
+    const sendLeaveEvent = function (websocket, lobbyId, userName, isInstant, isKick) {
+        websocket.send(JSON.stringify(ClientEvents.leaveEvent(lobbyId, userName, isInstant, isKick)));
+    }
+    /***
+     * Updates the connection that the server stores for this client so they can still communicate.
+     * @param {WebSocket} websocket
+     * @param {string} user
+     */
+    const sendUpdateConnectionEvent = function (websocket, user) {
+        websocket.send(JSON.stringify(ClientEvents.updateConnectionEvent(user)));
+    }
+    /***
+     * Requests the server to send back information about the given lobby.
+     * @param {WebSocket} websocket
+     * @param {string} lobbyId
+     */
+    const sendLobbyRequestEvent = function (websocket, lobbyId) {
+        websocket.send(JSON.stringify(ClientEvents.lobbyRequestEvent(lobbyId)));
+    }
+
+    /***
+     * Receive an event of type 'init'.
+     * @param {{type: string, lobby_id: string}} data
+     */
+    const receiveInit = function (data) {
+        window.location.replace(window.location.href.split("?")[0] + "?lobby=" + data["lobby_id"]);
+    }
+    /***
+     * Receive an event of type 'invite'.<br>
+     * If the receiving user is the intended user a modal will be shown asking if he accepts the invitation.
+     * @param {{type: string, lobby: string, to: string, from: string}} data
+     */
+    const receiveInvite = function (data) {
+        if (user === data["to"]) {
+            let text = `Do you want to accept the invitation from ${data["from"]} and join the lobby ${data["lobby"]}?`
+            UI.showInvitationModal(text);
+            invitedTo = data["lobby"];
+        }
+    }
+    /***
+     * Receive an event of type 'join'.<br>
+     * Information about the lobby is requested from the server in order to update the UI correctly.
+     * @param {WebSocket} websocket
+     * @param {string} lobbyId
+     */
+    const receiveJoin = function (websocket, lobbyId) {
+        Client.sendLobbyRequestEvent(websocket, lobbyId);
+    }
+    /***
+     * Receive an event of type 'lobby-info'.
+     * @param {{type: string, lobby: string[], admin: string}} data
+     * @param {WebSocket} websocket
+     * @param {string} lobbyId
+     */
+    const receiveLobbyInfo = function (data, websocket, lobbyId) {
+        UI.createPlayerCards(data["lobby"], data["admin"]);
+        registerKickBtnListeners(websocket, lobbyId);
+    }
+    // Receive an event of type 'kick'.
+    const receiveKickEvent = function () {
+        // TODO
+    }
+    // Receive an event of type 'start'.
+    const receiveStart = function () {
+        // TODO
+    }
+
+    /***
+     * Registers a 'ListenerRegistry.kickFromLobbyBtnListener' listener on each kick button.
+     * @param {WebSocket} websocket
+     * @param {string} lobbyId
+     */
+    function registerKickBtnListeners(websocket, lobbyId) {
+        let kickButtons = document.querySelectorAll("button[data-kick]");
+        for (let i = 0; i < kickButtons.length; i++) {
+            kickButtons[i].addEventListener("click", function () {
+                let userName = kickButtons[i].getAttribute("id").split("-")[1];
+                ListenerRegistry.kickFromLobbyBtnListener(websocket, lobbyId, userName);
+            });
+        }
+    }
+
+    return {
+        sendInitEvent: sendInitEvent,
+        sendInviteEvent: sendInviteEvent,
+        sendJoinEvent: sendJoinEvent,
+        sendLeaveEvent: sendLeaveEvent,
+        sendUpdateConnectionEvent: sendUpdateConnectionEvent,
+        sendLobbyRequestEvent: sendLobbyRequestEvent,
+        receiveInit: receiveInit,
+        receiveInvite: receiveInvite,
+        receiveJoin: receiveJoin,
+        receiveLobbyInfo: receiveLobbyInfo,
+        receiveKickEvent: receiveKickEvent,
+        receiveStart: receiveStart,
+        invitedTo: invitedTo,
+    }
+})();
+
+// Represents the UI of the client and provides methods to update certain parts of the UI.
+const UI = (function () {
+    const invitationModal = new bootstrap.Modal(document.querySelector("#invitation-modal"));
+
+    /***
+     * Shows the modal which is intended for invitations with the specified text.
+     * @param {string} text
+     */
+    const showInvitationModal = function (text) {
+        document.querySelector("#modal-text-content").textContent = text;
+        invitationModal.show();
+    }
+    /***
+     * Create cards for all players currently in the lobby.
+     * @param {string[]} lobby
+     * @param {string} admin
+     */
+    const createPlayerCards = function (lobby, admin) {
+        let playerDisplay = document.getElementById("invited-players");
+        playerDisplay.innerHTML = "";
+        for (let i = 0; i < lobby.length; i++) {
+            if (admin !== lobby[i]) {
+                createCard(lobby[i], "border-dark", true, playerDisplay);
+            } else {
+                createCard(lobby[i], "border-danger", false, playerDisplay);
+            }
+        }
+    }
+
+    /***
+     * Creates a card for a specific player with a specific border color.
+     * @param {string} playerName
+     * @param {string} borderColor
+     * @param {boolean} addKickBtn
+     * @param {HTMLElement} playerDisplay
+     */
+    function createCard(playerName, borderColor, addKickBtn, playerDisplay) {
+        let kickFromLobbyBtn = `<button type="button" class="btn btn-danger" data-kick id="kick-${playerName}">`
+            + `Kick from Lobby</button>`;
+        let playerCard = `<div class="card mb-3 ${borderColor} player-card" id="player-card-${playerName}"`
+            + ` style="width: 250px; height: 150px;"><div class="row g-0"><div class="col-md-2"><img src="user-logo.png"`
+            + ` class="img-fluid rounded-start" alt="user logo"></div><div class="col-md-8"><div class="card-body">`
+            + `<h5 class="card-title">${playerName}</h5><p class="card-text">Status: In Lobby<br><span`
+            + ` id="score-${playerName}" class="score-label" hidden>Score: 0</span></p>${addKickBtn ? kickFromLobbyBtn : ""}`
+            + `</div></div></div></div>`;
+        let col = document.createElement("div");
+        col.classList.add("col");
+        col.innerHTML = playerCard;
+        playerDisplay.appendChild(col);
+    }
+
+    return {
+        showInvitationModal: showInvitationModal,
+        createPlayerCards: createPlayerCards,
+    }
+})();
+
+// Stores all listeners that should be registered.
+const ListenerRegistry = (function () {
+    /***
+     * Listens for incoming messages on the websocket connection.
+     * @param {MessageEvent} event
+     * @param {WebSocket} websocket
+     * @param {string} lobbyId
+     */
+    const messageListener = function (event, websocket, lobbyId) {
+        const data = JSON.parse(event);
+        switch (data["type"]) {
+            case "init":
+                Client.receiveInit(data);
+                break;
+            case "invite":
+                Client.receiveInvite(data);
+                break;
+            case "join":
+                Client.receiveJoin(websocket, lobbyId);
+                break;
+            case "lobby-info":
+                Client.receiveLobbyInfo(data, websocket, lobbyId);
+                break;
+            case "kick":
+                Client.receiveKickEvent();
+                break;
+            case "start":
+                Client.receiveStart();
+                break;
+        }
+    }
+    /***
+     * Listens for a kick button being pressed on a player card.
+     * @param {WebSocket} websocket
+     * @param {string} lobbyId
+     * @param {string} userName
+     */
+    const kickFromLobbyBtnListener = function (websocket, lobbyId, userName) {
+        Client.sendLeaveEvent(websocket, lobbyId, userName, true, true);
+        window.location.replace(window.location.href.split("?")[0] + "?lobby=" + lobbyId);
+    }
+    /***
+     * Listens for a websocket connection to open.
+     * @param {WebSocket} websocket
+     * @param {string | null} lobbyId
+     */
+    const openConnectionListener = function (websocket, lobbyId) {
+        if (lobbyId == null) {
+            Client.sendInitEvent(websocket, user);
+        } else {
+            Client.invitedTo = null;
+            Client.sendUpdateConnectionEvent(websocket, user);
+            Client.sendLobbyRequestEvent(websocket, lobbyId);
+        }
+    }
+    /**
+     * Listens for the page to be reloaded.
+     * @param {WebSocket} websocket
+     * @param {string | null} lobbyId
+     */
+    const beforeunloadListener = function (websocket, lobbyId) {
+        if (Client.invitedTo == null && lobbyId != null) {
+            Client.sendLeaveEvent(websocket, lobbyId, user, false, false);
+        }
+    }
+
+    return {
+        messageListener: messageListener,
+        kickFromLobbyBtnListener: kickFromLobbyBtnListener,
+        openConnectionListener: openConnectionListener,
+        beforeunloadListener: beforeunloadListener,
+    }
+})();
+
+// "Entry point" for this file
+window.addEventListener("DOMContentLoaded", function () {
     const websocket = new WebSocket("ws://localhost:8001");
-    let modal = new bootstrap.Modal(document.querySelector("#invitation-modal"));
-    init(websocket, lobby_id);
-    sendInvite(websocket, lobby_id);
-    acceptInvite(websocket, modal, lobby_id);
-    receive(websocket, modal, lobby_id);
-    beforeUnload(websocket, lobby_id);
-    sendStartEvent(websocket, lobby_id);
+    const lobbyId = new URLSearchParams(window.location.search).get("lobby");
+    window.addEventListener("beforeunload", function () {
+       ListenerRegistry.beforeunloadListener(websocket, lobbyId);
+    });
+    websocket.addEventListener("open", function () {
+       ListenerRegistry.openConnectionListener(websocket, lobbyId);
+    });
+    websocket.addEventListener("message", function (event) {
+        ListenerRegistry.messageListener(event, websocket, lobbyId);
+    });
 });
 
-function beforeUnload(websocket, lobby_id) {
-    if (!lobby_id)
-        return;
-    window.addEventListener("beforeunload", () => {
-        if (INVITED_TO == null) {
-            leaveLobby(websocket, lobby_id, false, null, false);
-        }
-    });
-}
+
+//****************************
+//old
+//****************************
+
 
 function acceptInvite(websocket, modal, lobby_id) {
     document.getElementById("invitation-accept").addEventListener("click", () => {
@@ -34,17 +395,6 @@ function acceptInvite(websocket, modal, lobby_id) {
         leaveLobby(websocket, lobby_id, true, null, false);
         window.location.replace(window.location.href.split("?")[0] + "?lobby=" + INVITED_TO);
     });
-}
-
-function leaveLobby(websocket, lobby_id, isInstant, userName, isKick) {
-    let leaveEvent = {
-        "type": "leave",
-        "lobby": lobby_id,
-        "user": userName ? userName : user,
-        "instant": isInstant,
-        "kick": isKick
-    }
-    websocket.send(JSON.stringify(leaveEvent));
 }
 
 function sendInvite(websocket, lobby_id) {
@@ -108,93 +458,6 @@ function kickModalEvent(websocket) {
         sleep(5);
         websocket.send(JSON.stringify({"type": "init", "user": user}));
     });
-}
-
-function registerListeners(websocket, lobby_id) {
-    let elements = document.querySelectorAll("button[data-kick]")
-    for (let i = 0; i < elements.length; i++) {
-        elements[i].addEventListener("click", () => {
-            leaveLobby(websocket, lobby_id, true,
-                elements[i].getAttribute("id").split("-")[1], true);
-            window.location.replace(window.location.href.split("?")[0] + "?lobby=" + lobby_id);
-        });
-    }
-}
-
-function init(websocket, lobby_id) {
-    let init = {
-        "type": "init",
-        "user": user
-    }
-    console.log(lobby_id);
-    websocket.addEventListener("open", () => {
-        if (!lobby_id) {
-            websocket.send(JSON.stringify(init));
-        } else {
-            INVITED_TO = null;
-            updateConnection(websocket);
-            requestLobbyInfo(websocket, lobby_id);
-        }
-    });
-}
-
-function updateConnection(websocket) {
-    let updateConnection = {
-        "type": "update",
-        "user": user,
-    }
-    websocket.send(JSON.stringify(updateConnection));
-}
-
-function requestLobbyInfo(websocket, lobby_id) {
-    let lobbyRequest = {
-        "type": "request",
-        "lobby": lobby_id
-    }
-    websocket.send(JSON.stringify(lobbyRequest));
-}
-
-function updateUI(lobby, admin) {
-    let playerDisplay = document.getElementById("invited-players");
-    playerDisplay.innerHTML = "";
-    if (lobby.length > 1 && document.getElementById("start-btn").hasAttribute("hidden") && user === admin)
-        document.getElementById("start-btn").removeAttribute("hidden");
-
-    for (let i = 0; i < lobby.length; i++) {
-        if (admin !== lobby[i]) {
-            createCard(lobby[i], "", admin);
-        } else {
-            createCard(lobby[i], "border-danger", admin);
-        }
-    }
-}
-
-function createCard(playerName, backgroundColor, admin) {
-    let kickFromLobbyBtn = "";
-    if (user === admin && playerName !== user)
-        kickFromLobbyBtn = `<button type='button' class='btn btn-danger' data-kick
-                            id='kick-${playerName}'>Kick from Lobby</button>`;
-    let innerHTML = ` <div class='card mb-3 ${backgroundColor} player-card' id='player-card-${playerName}' style="width: 250px; height: 150px;">` +
-        "                            <div class='row g-0'>" +
-        "                                <div class='col-md-2'>" +
-        "                                    <img src='user-logo.png'" +
-        "                                         class='img-fluid rounded-start'" +
-        "                                         alt='user logo'></div>" +
-        "                                <div class='col-md-8'>" +
-        "                                    <div class='card-body'>" +
-        `                                       <h5 class='card-title'>${playerName}</h5>` +
-        "                                        <p class='card-text'>Status: In Lobby" +
-        `<br><span id="score-${playerName}" class="score-label" hidden>Score: 0</span></p>` +
-        `${kickFromLobbyBtn}` +
-        "                                    </div>" +
-        "                                </div>" +
-        "                            </div>" +
-        "                        </div>";
-    let playerDisplay = document.getElementById("invited-players");
-    let col = document.createElement("div");
-    col.classList.add("col");
-    col.innerHTML = innerHTML;
-    playerDisplay.appendChild(col);
 }
 
 function sleep(seconds) {
